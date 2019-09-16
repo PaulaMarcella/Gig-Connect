2d5dee6fc0c1493ea5971dff1666dac9775188ae
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-09-16 19:06:15 +0100

    user to be finished

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@
 
 const { Router } = require('express');
 const router = Router();
-const User = require('../models/user');
+const User = require('./../models/user');
 
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -16,17 +16,22 @@ router.get('/user', (req, res, next) => {
 */
 
 router.get('/user', (req, res, next) => {
-  const userId = req.session.user._id;
+  //const userId = req.session.user._id;
   //console.log(req.session);
-  User.findById(userId)
-    .then(() => {
-      res.render('user');
+  User.find()
+    .then((userlist) => {
+      const data = {
+        userlist
+      };
+      res.render('user', data);
 
     })
     .catch(error => {
       console.log('User not found', error);
     });
 });
+
+
 
 
 module.exports = router;
