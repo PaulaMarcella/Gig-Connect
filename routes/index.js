139667c51bb9917130f220600b41c80b684bfca8
Tiago Silva <tiagosilva94@outlook.com>
2019-09-17 18:35:20 +0100

    Add small changes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@
 
 const { Router } = require('express');
 const router = Router();
+const Event = require('../models/event');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'GigConnect' });
 });
+
+// Work in progress:
+// router.get('/', (req, res, next) => {
+//   Event.findById(req.body.)
+//   .then((event) => {
+//     res.render('index', {event});
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+// });
 
 module.exports = router;
