cfcf944289a532b3e1af135c08320255b3155381
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-09-17 18:31:48 +0100

    added movie list

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,20 @@
 
 const { Router } = require('express');
 const router = Router();
+const Event = require('../models/event');
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'GigConnect' });
+  Event.find()
+  .then(eventList => {
+    const data = {
+      eventList
+    };
+    console.log(data);
+    res.render('index', data);
+  })
+  .catch(error => {
+    next(error);
+  });
 });
 
 module.exports = router;
