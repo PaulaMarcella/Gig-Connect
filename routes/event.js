ad0d6c75efd5ee464a6433052dfacb07855d1fd1
Tiago Silva <tiagosilva94@outlook.com>
2019-09-17 14:47:47 +0100

    Add code for group testing

--- routes/event.js
+++ routes/event.js
@@ -7,9 +7,9 @@ const Event = require('../models/event');
 router.get('/event', (req, res, next) => {
     res.render('event/event');
     console.log(req.body);
-  });
+});
 
-router.post('/event', (req, res) => {
+router.post('/event', (req, res, next) => {
   // Creating an event
   console.log("The event object:", req.body);
   const eventName = req.body.event;
@@ -18,7 +18,7 @@ router.post('/event', (req, res) => {
   const location = req.body.location; 
   const ticketURL = req.body.ticket; 
   const imageURL = req.body.image;
-
+  
   Event.create({
     eventName,
     description,
@@ -27,14 +27,37 @@ router.post('/event', (req, res) => {
     ticketURL,
     imageURL
   });
-  // Redirecting to the route of the created event
-//   res.redirect('/aRoute/'+ "eventId");
+
+  // TODO 01
+  // Find the event in the DB, it's id, and redirect to route
+
+
+  // Redirecting to the route of the created event through id (route + eventId)
+  res.redirect('/eventPage/');
 });
 
-router.get("/aRoute/:id",(req,res,next)=>{
-  // req.params (Gets information from the url)
-  // Find by id a specific event and render details
-  res.render(/*A VIEW*/);
+router.get('/eventPage', (req, res, next) => {
+  res.render('event/eventPage');
+  //console.log(req.body);
 });
+
+router.get('/eventPage', (req, res, next) => {
+  Event.findById({_id: req.query})
+    .then((event) => {
+      //res.render('/event/eventPage');
+      console.log(event);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
+// TODo 02
+// router.get("/aRoute/:id",(req,res,next)=>{
+//   // req.params (Gets information from the url)
+//   // Find by id a specific event and render details
+//   res.render(/*A VIEW*/);
+// });
 
 module.exports = router;
