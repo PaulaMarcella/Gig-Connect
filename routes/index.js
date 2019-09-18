51872805c5674825a14948cfdecd72d57f6f68fa
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-09-18 20:06:37 +0100

    search form updated

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,38 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.get('/event-search-location', (req, res, next) => {
-  const searchResult = req.query.location;
-  console.log("QUERY RESULT",searchResult);
-  Event.find({location: searchResult})
+// router.get('/event-search-location', (req, res, next) => {
+//   const searchResult = req.query.location;
+//   console.log("QUERY RESULT",searchResult);
+//   Event.find({location: searchResult})
+//   .then(eventList => {
+//     const data = {
+//       eventList
+//     };
+//     console.log(data);
+//     res.render('index', data);
+//   })
+//   .catch(error => {
+//     next(error);
+//   });
+// });
+
+
+router.get('/event-search', (req, res, next) => {
+  //const searchResult = req.query.location;
+  //console.log("QUERY RESULT",searchResult);
+  Event.find({
+    genre: {$regex : `${ req.query.search }`} || undefined ,
+    city: {$regex : `${ req.query.search }`} || undefined,
+    artists: {$regex : `${ req.query.search }`} || undefined,
+    eventName: {$regex : `${ req.query.search }`} || undefined
+  })
   .then(eventList => {
-    const data = {
-      eventList
-    };
-    console.log(data);
-    res.render('index', data);
+    // const data = {
+    //   eventList
+    // };
+    console.log("HIIII" + {eventList});
+    res.render('index', {eventList});
   })
   .catch(error => {
     next(error);
